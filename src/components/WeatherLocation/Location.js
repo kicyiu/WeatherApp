e7135ecbd464dc5fa48fbbfb7ae721b202856bd2
Alberto Tsang <atsang@ATSANGM.local>
2019-06-12 09:58:47 -0400

    segundo commit

--- src/components/WeatherLocation/Location.js
+++ src/components/WeatherLocation/Location.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
 
-const Location = ({ city }) => {
-
-    return (
-        <div>
+const Location = ({ city }) => (
+        <div className="locationCont">
             <h1>{ city }</h1>
         </div>
-    )
-    
-}
+);
 
 Location.propTypes = {
     city: PropTypes.string.isRequired,
