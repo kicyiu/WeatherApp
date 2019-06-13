3f56e11dc22b856afd4ebf433f5358ca3f33c562
kicyiu <albertokicyiu@gmail.com>
2019-06-12 22:27:44 -0400

    cambios menores

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
 import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY, FOG } from '../../../constants/weathers';
 import './styles.css';
 
-const WeatherData = () => (
+const WeatherData = ({ data: { temperature, weatherState, humidity, wind } }) => (
     <div className="weatherDataCont">
         <WeatherTemperature
-            temperature={20}
-            weatherState={SUN}
+            temperature={temperature}
+            weatherState={weatherState}
         />
-        <WeatherExtraInfo humidity={80} wind={"10 m/s"}/>
+        <WeatherExtraInfo humidity={humidity} wind={wind} />
     </div>
-);
+)
+    
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
+}
+
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
+import { CLOUD, CLOUDY, SUN, RAIN, SNOW, WINDY, FOG } from '../../constants/weathers';
 
-const WeatherLocation = () => (
-    <div className="weatherLocationCont">
-        <Location city={"Beijing"} ></Location>
-        <WeatherData></WeatherData>
-    </div>
-);
+const data = {
+    temperature: 5,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s'
+}
+
+class WeatherLocation extends Component {
+    
+    render() {
+        return (
+            <div className="weatherLocationCont">
+                <Location city={"Beijing"} ></Location>
+                <WeatherData data={data}></WeatherData>
+            </div>
+        );
+    }
+};
 
 export default WeatherLocation;
