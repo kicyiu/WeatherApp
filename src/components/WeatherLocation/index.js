33ef0449091a83a61b64ac10442d3396fc6f13fc
Alberto Tsang <atsang@ATSANGM.local>
2019-06-17 18:01:18 -0400

    agregado operador tenario al momento d llegar los datos

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -3,16 +3,9 @@ import convert from 'convert-units';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
-import { SUN } from '../../constants/weathers';
 import transformWeather from './../../services/transformWeather';
 import { api_weather } from './../../constants/api_url';
 
-const data = {
-    temperature: "5",
-    weatherState: SUN,
-    humidity: 10,
-    wind: '10 m/s'
-}
 
 class WeatherLocation extends Component {
     
@@ -20,7 +13,7 @@ class WeatherLocation extends Component {
         super();
         this.state = {
             city: "Shenzhen",
-            data: data
+            data: null
         }
         console.log("constructor");
     }
@@ -58,8 +51,7 @@ class WeatherLocation extends Component {
         return (
             <div className="weatherLocationCont">
                 <Location city={ city } ></Location>
-                <WeatherData data={ data }></WeatherData>
-                <button onClick={ this.handleUpdateClick }>Actualizar</button>
+                {data? <WeatherData data={ data }></WeatherData> : "Cargando..."}
             </div>
         );
     }
