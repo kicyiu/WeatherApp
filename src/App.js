2ad46a144f687391cfe615797acdaafd7140d241
kicyiu <albertokicyiu@gmail.com>
2019-07-02 23:38:38 -0400

    cambios menores

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Component} from 'react';
 import LocationList from './components/locationList';
 import './App.css';
 
+const cities = [
+  'Buenos Aires, ar',
+  'Hunan, cn',
+  'Osaka, jp',
+  'Caracas, ve',
+  'Madrid, es',
+  'San Francisco, us'
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App"> 
-        <LocationList></LocationList>
+        <LocationList cities={ cities }></LocationList>
       </div>
     );
   }
--- src/components/locationList.js
+++ src/components/locationList.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherLocation from './WeatherLocation';
 
-const LocationList = () => (
+const LocationList = ({ cities }) => {
+    console.log(cities);
+    return(
     <div>
         <WeatherLocation city="Hunan, cn"/>
-        <WeatherLocation city="Caracas, ven"/>
-        <WeatherLocation city="Buenos Aires, ar"/>
-    </div>
-);
+        <WeatherLocation city="Kyoto, jp"/>
+        <WeatherLocation city="Washington, us"/>
+        <WeatherLocation city="Madrid, es"/>
+    </div>);
+};
+
+LocationList.propTypes = {
+    cities: PropTypes.array.isRequired,
+}
 
 export default LocationList;
