c2d0647d5983c29b99284f3673cb8809d9bf2081
kicyiu <albertokicyiu@gmail.com>
2019-07-06 16:37:32 -0400

    agregado texto de prueba

--- src/components/locationList.js
+++ src/components/locationList.js
@@ -2,19 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WeatherLocation from './WeatherLocation';
 
-const LocationList = ({ cities }) => {
-    console.log(cities);
-    return(
+const strToComponents = cities => (
+    cities.map( city => <WeatherLocation city={ city } /> )
+);
+
+const LocationList = ({ cities }) => (
     <div>
-        <WeatherLocation city="Hunan, cn"/>
-        <WeatherLocation city="Kyoto, jp"/>
-        <WeatherLocation city="Washington, us"/>
-        <WeatherLocation city="Madrid, es"/>
-    </div>);
-};
+        { strToComponents(cities) }
+    </div>
+);
 
 LocationList.propTypes = {
     cities: PropTypes.array.isRequired,
 }
+
+
+//texto de prueba
 
 export default LocationList;
